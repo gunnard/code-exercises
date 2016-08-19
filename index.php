335f43b721dc0c8633144719dbff368ec7388566
Gunnard <gunnard@blendimc.com>
2016-08-19 11:23:31 -0500

    added validation, css fixes, form output to page

--- index.php
+++ index.php
@@ -1,36 +1,40 @@
 <?php 
 $ipAddy = $_SERVER['REMOTE_ADDR'];
 
-function checkInput($str) {
+/**
+ * Simple form sanitization
+ * 
+ * @param str String to be stripped of html tags and slashes
+ * @return str Clean string
+ */
+function checkInput($str) 
+{
     $str = @strip_tags($str);
     $str = @stripslashes($str);
     return $str;
 }
 
-
 if ($_POST) {
-
-    echo '</br></br></br>';
-    echo '<h1>Results:</h1>';
-    echo '<h2>Name: '.checkInput($_POST['name']).'</h2>';
-    echo '<h2>Email: '.checkInput($_POST['email']).'</h2>';
-    echo '<h2>Phone: '.checkInput($_POST['thePhone']).'</h2>';
-    echo '<h2>Message: '.checkInput($_POST['message']).'</h2>';
-    echo '<hr />';
+    $output = '<div class="form-output">';
+    $output .= '<h1>Results:</h1>';
+    $output .= '<h2>Name: '.checkInput($_POST['name']).'</h2>';
+    $output .= '<h2>Email: '.checkInput($_POST['email']).'</h2>';
+    $output .= '<h2>Phone: '.checkInput($_POST['thePhone']).'</h2>';
+    $output .= '<h2>Message: '.checkInput($_POST['message']).'</h2>';
+    $output .= '</div>';
+    $output .= '<hr />';
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
+    <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>The Bestest Form</title>
+    <title>The Great Form</title>
     <!-- JQuery -->        
-    <script   src="https://code.jquery.com/jquery-3.1.0.slim.min.js"   integrity="sha256-cRpWjoSOw5KcyIOaZNo4i6fZ9tKPhYYb6i5T9RSVJG8="   crossorigin="anonymous"></script>
-    <!-- Latest compiled and minified CSS -->
+<script   src="https://code.jquery.com/jquery-3.1.0.slim.min.js"   integrity="sha256-cRpWjoSOw5KcyIOaZNo4i6fZ9tKPhYYb6i5T9RSVJG8="   crossorigin="anonymous"></script>
+<!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <!-- Optional theme -->
@@ -38,14 +42,15 @@ if ($_POST) {
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
 <style>
-.form-template {
-    padding-top:70px;
+    .form-template, .form-output {
+        min-width: 290px;
+        max-width: 600px;
+        margin: 0 auto;
 }
 .phone-number .col-xs-3::after{
- content: "-";
- position:absolute;
+    content: "-";
+    position:absolute;
     right: 5px;
     color: black;
     border: 0px solid;
@@ -63,7 +68,7 @@ if ($_POST) {
 </style>
 </head>
 <body>
-<nav class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -73,65 +78,74 @@ if ($_POST) {
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="#">Earthling Code Test</a>
-        </div>
+        </div><!-- /.navbar-header -->
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
             <li><a href="http://www.gunnard.org">Gunnard</a></li>
           </ul>
         </div><!--/.nav-collapse -->
-      </div>
+      </div><!-- /.container -->
     </nav>
+
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1>Hello,<?php echo $ipAddy; ?>!</h1>
+        <h2>Hello, person from: <?php echo $ipAddy; ?>!</h2>
         <p>Hi!, please fill out this form and I guarentee that you will not be added to any email lists at all!</p>
       </div>
     </div>
-
     <div class="container">
-
-      <div class="form-template">
-        <h1>Please fill out this form.</h1>
-            <form name="info_form" action="#" onsubmit="return validateForm()" method="post">
-        <div class="form-group">
-            <label for="name">Name:</label>
-                <input type="text" class="form-control" id="name" name="name">
-        </div>
-        <div class="form-group">
-            <label for="email">Email:</label>
-                <input type="text" class="form-control" id="email" name="email">
-        </div>
-            <label for="phone">Phone:</label>
-<div class="form-group phone-number">
+        <?php if ($output) {
+            echo $output;
+        } ?>  
+        <div class="form-template">
+            <h1>Please fill out this form.</h1>
+            <form name="info-form" action="#" onsubmit="return validateForm()" method="post">
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" class="form-control" id="name" name="name">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="text" class="form-control" id="email" name="email">
+                </div>
+                <label for="phone">Phone:</label>
+                <div class="form-group phone-number">
                     <div class="col-xs-3">
-                        <input type="tel" id="phone1" name="phone" class="form-control" value="" size="3" maxlength="3" title="">
+                        <input type="tel" placeholder="608" id="phone1" name="phone" class="form-control" value="" size="3" maxlength="3">
                     </div>
-
                     <div class="col-xs-3">
-                        <input type="tel" id="phone2" name="phone" class="form-control" value="" size="3" maxlength="3" title="">
+                        <input type="tel" placeholder="123" id="phone2" name="phone" class="form-control" value="" size="3" maxlength="3">
                     </div>
-
                     <div class="col-xs-4">
-                        <input type="tel" id="phone3"name="phone" class="form-control" value="" size="4" maxlength="4" title="">
+                        <input type="tel" placeholder="1234" id="phone3"name="phone" class="form-control" value="" size="4" maxlength="4">
                         <input type="hidden" id="thePhone"name="thePhone" class="form-control" value="" size="4" maxlength="4" title="">
                     </div>
+                </div><!-- /.phone-number -->
+                <div class="form-group">
+                    <label for="email">Message:</label>
+                    <textarea class="form-control" id="message" name="message" rows="4" cols="50"></textarea>           
                 </div>
-        <div class="form-group">
-            <label for="email">Message:</label>
-                <textarea class="form-control" id="message" name="message" rows="4" cols="50"></textarea>           
-        </div>
-<button type="button submit" class="btn btn-primary">Submit</button>
-   </form>
-      </div>
+                <button type="button submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div> <!-- /.form-template -->
     </div><!-- /.container -->
 <script>
+/**
+* Validates email address
+*
+* @param email string Email to validate
+* @return bool 
+*/
 function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+    var userEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return userEmail.test(email);
 }
 
+/**
+* Validates all input fields in the form alerting with a compilation of errors, if any
+*/
 function validateForm() {
     var theName = $('#name').val();
     var theEmail = $('#email').val();
@@ -153,7 +167,6 @@ function validateForm() {
     }
     if (thePhone == null || thePhone == "" || thePhone.length < 10) {
         message += "Oops! Your Phone Number must be filled out\n";
-
         passed = false;
     }
     if (theMessage == null || theMessage == "") {
@@ -165,11 +178,8 @@ function validateForm() {
         return false;
     } else {
         $('#thePhone').val(thePhone);
-        alert('here');
     }
 }
-
-
 </script>
 </body>
 </html>
