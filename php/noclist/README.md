48743091717a9450a4c69520a6a69f4f53e2718a
Gunnard Engebreth <gunnard@gmail.com>
2019-06-21 14:34:25 -0500

    finalized noclist

--- php/noclist/README.md
+++ php/noclist/README.md
@@ -1,1 +1,4 @@
 Readme for NOCLIST
+
+Simply run 'php noclist.php' while the BADSECserver is running at htt://0.0.0.0:8888. The output will
+show on the command line
--- php/noclist/noclist.php
+++ php/noclist/noclist.php
@@ -1,39 +1,82 @@
 <?php
 
-function getApiUsers($authToken) {
+function getApiUsers() {
 
-	$apiUsersUrl = 'http://0.0.0.0:8888/users';
+	//Set users endpoint and initialize curl
+	$baseUrl = 'http://0.0.0.0:8888';
+	$endPoint = '/users';
+	$apiUsersUrl = $baseUrl . $endPoint;
 	$ch = curl_init();
 
-	print 'Auth Token: '. $authToken ."\r\n";
-	$checksum = hash('sha256', ($authToken . "/users"));
+	//Set authToken to null and attempt to 0;
+	$authToken = '';
+	$attempt = 0;
 
-	$headers = [ 
-		'X-Request-Checksum: '. $checksum
-	];
+	//get the auth token
+	//try 3 times
+	while ($authToken == '' && $attempt <3 ){
+		$authToken = getApiAuth();
+		$attempt++;
+	}
+	unset($attempt);
 
-	curl_setopt($ch, CURLOPT_URL,$apiUsersUrl);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$result = curl_exec($ch);
-	curl_close ($ch);
-	return $result;
+	//If authToken exists, call users endpoint
+	$result = null;
+	$attempt = 0;
+	if ($authToken) {
+		$checksum = hash('sha256', ($authToken . "/users"));
+
+		$headers = [ 
+			'X-Request-Checksum: '. $checksum
+		];
+
+		curl_setopt($ch, CURLOPT_URL,$apiUsersUrl);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		while ($result == null && $attempt <3) {
+			$result = curl_exec($ch);
+			$attempt++;
+		}
+		curl_close ($ch);
+	} else {
+		exit(1);
+	}
+	unset($authToken);
+	unset($attempt);
+	if ($result) {
+		return $result;
+	} else {
+		exit(1);
+	}
 }
 
 function getApiAuth() {
-	$ch = curl_init();
-	$apiAuthUrl = 'http://0.0.0.0:8888/auth';
+	//Set base url and endpoint
+	$baseUrl = 'http://0.0.0.0:8888';
+	$endPoint = '/auth';
+	$apiAuthUrl = $baseUrl . $endPoint;
+
+	//Get header information from url
 	$result = get_headers($apiAuthUrl, 1);
-	$theToken = $result['Badsec-Authentication-Token'];
-	return $theToken;
+
+	//if anything but '200 OK' then die
+	if (preg_match("/200 OK/", $result[0])) {
+		$theToken = $result['Badsec-Authentication-Token'];
+		unset($result);
+		return $theToken;
+	} else {
+		exit(1);
+	}
 }
 
-$authToken = getApiAuth();
-$users = null;
-while ($users == null) {
-	echo "doing it \r\n";
-	$users = getApiUsers($authToken);
+//main call to getApiUsers
+$users = getApiUsers();
+
+//if conenction worked and users exist, explode on line break and json encode
+if ($users) {
+	$users = json_encode(explode("\n", $users));
+	echo($users);
+	exit(0);
+} else {
+	exit(1);
 }
-$users = json_decode(json_encode($users));
-echo $users;
-// EOF
